adbf80d687a9dea70e7a6d9aeab1f6203069bff8
SangatiThanmayee <reddysangatithanmayee@gmail.com>
2025-08-16 15:10:47 +0530

    added solution for question3

--- question3/app.js
+++ question3/app.js
@@ -8,13 +8,13 @@ let income = 60000;
 if (loanamount > 1000000 && creditscore > 800) {
     interestrate = 7;
     if (isgovemp) {
-        interestrate -= 5;
+        interestrate -=0.5;
     }
 }
 else if (creditscore >= 600 && creditscore <= 800 && income > 50000) {
     interestrate = 9;
     if (isgovemp) {
-        interestrate -= 5;
+        interestrate -=0.5;
     }
 }
 else
