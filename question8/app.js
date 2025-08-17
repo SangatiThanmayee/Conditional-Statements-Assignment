a694e071a75b455a3b12a4b5fd13ec292b86e2d6
SangatiThanmayee <reddysangatithanmayee@gmail.com>
2025-08-17 12:46:40 +0530

    added solution for question8

---
+++ question8/app.js
@@ -0,0 +1,63 @@
+let category = "lunch";
+let item = "vegmeals";
+let price = 0;
+let quantity = 3;
+// assigning the price according to category and sub items//
+switch (category) {
+    case "breakfast":
+        switch (item) {
+            case "idli":
+                price = 30;
+                break;
+            case "dosa":
+                price = 40;
+                break;
+            case "paratha":
+                price = 50;
+                break;
+            //if item is not available//   
+            default:
+                console.log("item is not available");
+        }
+        break;
+    case "lunch":
+        switch (item) {
+            case "vegmeals":
+                price = 80;
+                break;
+            case "vegbiryani":
+                price = 100;
+                break;
+            case "chickenbiryani":
+                price = 150;
+                break;
+            //if item is not available//   
+            default:
+                console.log("item is not available");
+        }
+        break;
+    case "dinner":
+        switch (item) {
+            case "vegmeals":
+                price = 80;
+                break;
+            case "chapathi":
+                price = 40;
+                break;
+            case "vegcurry":
+                price = 50;
+                break;
+            //if item is not available//   
+            default:
+                console.log("item is not available");
+        }
+        break;
+    //if category is not available//
+    default:
+        console.log("category is not available");
+}
+
+
+//final output//
+let total = price * quantity;
+console.log(`total:${total}`);
